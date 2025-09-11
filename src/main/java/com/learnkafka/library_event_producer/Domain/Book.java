fd4a9987989afab5bbf5c0fbc93541280ae7101d
nguetcheu <dnguetcheu@gmail.com>
2025-09-11 13:01:09 +0200

    Update Book model to enforce non-blank validation on bookName field

--- src/main/java/com/learnkafka/library_event_producer/Domain/Book.java
+++ src/main/java/com/learnkafka/library_event_producer/Domain/Book.java
@@ -3,5 +3,5 @@ package com.learnkafka.library_event_producer.Domain;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-public record Book(@NotNull Integer bookId, @NotNull String bookName,@NotBlank String bookAuthor) {
+public record Book(@NotNull Integer bookId, @NotBlank String bookName,@NotBlank String bookAuthor) {
 }
